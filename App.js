ca741e7dce6aa3df723ae7dc2daf309c6cb8fb83
bipin <beepyn98166@gmail.com>
2023-05-30 23:56:38 +0545

    updated search

--- App.js
+++ App.js
@@ -33,6 +33,7 @@ app.get("/view", (req, res) => {
     res.render("view")
 });                                 
 
+//routing for add student
 app.get("/addStudent", (req, res) => {
    //fetching the data from the form
    const {name,phone,email,gender} = req.query;
@@ -40,20 +41,88 @@ app.get("/addStudent", (req, res) => {
    //sanitizing the data 
         let qry = "SELECT * FROM crud WHERE email = ? or phone = ?";
         mysql.query(qry, [email,phone], (err, results) => {
-        if (err) throw err;
+        if (err) 
+            throw err;
         else{ 
-            //insert query to insert the data into the database
-            let qry2 = "INSERT INTO crud VALUES (?,?,?,?)";
-            mysql.query(qry2, [name, phone, email, gender], (err, results) => {
-                if (results.affectedRows > 0)
-                    res.render("add", { mesg: true }),
-                    console.log("Data inserted successfully");
-                else
-                    res.render("add", { mesg: false })
-            });
-        }
+                if (results.length > 0){
+                     res.render("add", { checkmesg: true });
+                }
+                else{
+                    //insert query to insert the data into the database
+                    let qry2 = "INSERT INTO crud VALUES (?,?,?,?)";
+                    mysql.query(qry2, [name, phone, email, gender], (err, results) => {
+                    if (results.affectedRows > 0){
+                            res.render("add", { mesg: true });
+                        }
+                    });
+                }
+            }
     })
 });
+
+//routing for search student
+app.get("/searchstudent", (req, res) => {
+    //fetching the data from the form
+    const {phone} = req.query;
+    //select query to fetch the data from the database
+    let qry = "SELECT * FROM crud WHERE phone = ?";
+    mysql.query(qry, [phone], (err, results) => {
+        if (err) 
+            throw err;
+        else
+            if (results.length > 0){
+                res.render("search", { mesg1: true, mesg2: false });
+            }
+            else{
+                res.render("search", { mesg1: false, mesg2: true });
+            }
+    });
+
+});
+
+
+//routing for update student
+app.get("/updatesearch", (req, res) => {
+    const {phone} = req.query;
+    //select query to fetch the data from the database
+    let qry = "SELECT * FROM crud WHERE phone = ?";
+    mysql.query(qry, [phone], (err, results) => {
+        if (err) 
+            throw err;
+        else
+            if (results.length > 0){
+                res.render("update", { mesg1: true, mesg2: false, data:results });
+            }
+            else{
+                res.render("update", { mesg1: false, mesg2: true });
+            }
+    });
+
+});
+
+//routing for update student
+app.get("/updatestudent", (req, res) => {
+    //fetching the data from the form
+    const{phone}=req.query;
+    const{username}  = req.query;
+    const {gender} = req.query;
+    let qry = "UPDATE crud SET name=?, gender=? WHERE phone=?";
+    mysql.query(qry, [name,gender,phone], (err, results) => {
+        if (err) 
+            throw err;
+        else
+            if (results.affectedRows > 0){
+                res.render("update", { umesg: true  });
+            }
+            else{
+                res.render("update", { umesg: false });
+            }
+    });
+});
+
+
+
+
 
 //Create Server
 app.listen(port, (err) => {
